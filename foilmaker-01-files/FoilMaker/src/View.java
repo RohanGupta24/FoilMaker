4a65c77963f8e8a14fb544441c4ff069ed52004a
Rohan Gupta <gupta326@purdue.edu>
2016-10-31 21:18:23 -0400

    Updated View.java
    I have updated the View with comments and a few adjustments. I still
    need to create the necessary methods.

--- foilmaker-01-files/FoilMaker/src/View.java
+++ foilmaker-01-files/FoilMaker/src/View.java
@@ -13,10 +13,15 @@ import javax.swing.border.Border;
 
 public class View extends JFrame {
 
+    JFrame frame = new JFrame();
+
     JPanel mainPanel = new JPanel();
     CardLayout layout = new CardLayout();
 
     JPanel panelFirst = new JPanel();
+    JPanel subPanel1 = new JPanel(new GridLayout(1,2));
+    JPanel subPanel2 = new JPanel(new GridLayout(1,2));
+    JPanel subPanel3 = new JPanel(new GridLayout(1,2));
     Border p1 = BorderFactory.createTitledBorder("FoilMaker!");
     JButton buttonLogin = new JButton("Login");
     JButton buttonRegister = new JButton("Register");
@@ -26,32 +31,75 @@ public class View extends JFrame {
     JTextField usernameBox = new JTextField();
 
     JPanel panelSecond = new JPanel();
-    Border rest = BorderFactory.createTitledBorder("Bob");
+    JPanel secondSubPanel = new JPanel(new GridLayout(1,2));
     JButton buttonStartNewGame = new JButton("Start New Game");
     JButton buttonJoinAGame = new JButton("Join a Game");
 
     JPanel panelThird = new JPanel();
+    JPanel thirdSubPanel = new JPanel(new GridLayout(4,1));
     JLabel keyDescription = new JLabel("Others should use this key to join your game");
     JTextField key = new JTextField();
     JPanel participants = new JPanel();
     JButton buttonStartGame = new JButton("Start Game");
 
     JPanel panelFourth = new JPanel();
+    JPanel fourthSubPanel = new JPanel(new GridLayout(3,1));
     JLabel gameKeyInstructions = new JLabel("Enter the game key to join a game");
     JTextField gameKey = new JTextField();
     JButton buttonJoinGame = new JButton("Join Game");
 
     JPanel panelFifth = new JPanel();
+    JPanel fifthSubPanel = new JPanel(new GridLayout(1,0));
+    JLabel waitingLeader = new JLabel("Waiting for leader...");
+
+    JPanel panelSixth = new JPanel();
+    JPanel sixthSubPanel = new JPanel(new GridLayout(4,1));
+    JPanel wordIdentification = new JPanel();
+    JLabel wordClue = new JLabel();
+    JPanel suggestion = new JPanel();
+    JTextField suggestionBox = new JTextField();
+    JButton buttonSubmitSuggestion = new JButton("Submit Suggestion");
+    JLabel wordIdentifcationInstructions = new JLabel("What is the word for");
+
+    JPanel panelSeventh = new JPanel();
+    JPanel seventhSubPanel = new JPanel(new GridLayout(5,1));
+    JLabel pickOptionDescription = new JLabel("Pick your option below");
+    JRadioButton option1 = new JRadioButton();
+    JRadioButton option2 = new JRadioButton();
+    JRadioButton option3 = new JRadioButton();
+    ButtonGroup options = new ButtonGroup();
+    JButton buttonSubmitOption = new JButton("Submit Option");
+
+    JPanel panelEighth = new JPanel();
+    JPanel eightSubPanel = new JPanel(new GridLayout(3,1));
+    JPanel roundResult = new JPanel();
+    JPanel overallResults = new JPanel();
+    JButton nextRound = new JButton("Next Round");
 
     public View() {
-        panelFirst.setLayout(new GridLayout(3,2));
-        panelFirst.add(username);
-        String username = passwordBox.getText();
-        panelFirst.add(usernameBox);
-        panelFirst.add(password);
-        panelFirst.add(passwordBox);
+
+        //RENDER login
+        //Record username and password in server when user registers to login is successful the next time
+        panelFirst.setLayout(null);
+        panelFirst.add(subPanel1);
+        subPanel1.setSize(500,200);
+        subPanel1.add(username);
+        subPanel1.add(Box.createHorizontalStrut(5));
+        subPanel1.add(usernameBox);
+        subPanel1.setBounds(50,125,200,20);
+        panelFirst.add(subPanel2);
+        subPanel2.setSize(500,200);
+        subPanel2.add(password);
+        subPanel2.add(Box.createHorizontalStrut(5));
+        subPanel2.add(passwordBox);
+        subPanel2.setBounds(50,150,200,20);
+        panelFirst.add(subPanel3);
+        //IMPORTANT: This retrieves username and password to record in server
+        String username = usernameBox.getText();
         String password = passwordBox.getText();
-        panelFirst.add(buttonLogin);
+        Border rest = BorderFactory.createTitledBorder(username);
+        subPanel3.setBounds(50,300,200,20);
+        subPanel3.add(buttonLogin);
         buttonLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -61,7 +109,8 @@ public class View extends JFrame {
                 }
             }
         });
-        panelFirst.add(buttonRegister);
+        subPanel3.add(Box.createHorizontalStrut(1));
+        subPanel3.add(buttonRegister);
         buttonRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -72,63 +121,158 @@ public class View extends JFrame {
             }
         });
 
-        panelFirst.setBackground(Color.DARK_GRAY);
-
-        panelSecond.add(buttonStartNewGame);
+        panelSecond.setLayout(null);
+        panelSecond.add(secondSubPanel);
+        secondSubPanel.setSize(500,200);
+        secondSubPanel.setBounds(50,200,200,20);
+        secondSubPanel.add(buttonStartNewGame);
         buttonStartNewGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 layout.show(mainPanel, "3");
             }
         });
-        panelSecond.add(buttonJoinAGame);
+        secondSubPanel.add(buttonJoinAGame);
         buttonJoinAGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 layout.show(mainPanel, "4");
             }
         });
-        panelSecond.setBackground(Color.DARK_GRAY);
 
-        panelThird.add(keyDescription);
-        panelThird.add(key);
-        panelThird.add(participants);
+        //Display participants and key to user (from server)
+        panelThird.add(thirdSubPanel);
+        thirdSubPanel.setSize(500,200);
+        thirdSubPanel.setBounds(50,125,200,20);
+        thirdSubPanel.add(keyDescription);
+        thirdSubPanel.add(key);
+        key.setText("ypw"); //IMPORTANT: This text should change based on the server's response
+        key.setEditable(false);
+        thirdSubPanel.add(participants);
         Border forParticipants = BorderFactory.createTitledBorder("Participants");
         participants.setBorder(forParticipants);
         participants.setBackground(Color.orange);
-        panelThird.add(buttonStartGame);
+        thirdSubPanel.add(buttonStartGame);
         buttonStartGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                layout.show(mainPanel, "6");
             }
         });
-        panelThird.setBackground(Color.DARK_GRAY);
+        //Server should provide game key to user
+        panelFourth.add(fourthSubPanel);
+        fourthSubPanel.add(gameKeyInstructions);
+        fourthSubPanel.add(gameKey);
+        String checkKeyValidity = gameKey.getText(); //The server should check to see if the game key is valid
+        fourthSubPanel.add(buttonJoinGame);
+        buttonJoinGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                layout.show(mainPanel, "5");
+            }
+        });
 
-        panelFourth.add(gameKeyInstructions);
-        panelFourth.add(gameKey);
-        panelFourth.add(buttonJoinGame);
-        panelFourth.setBackground(Color.DARK_GRAY);
+        //Difficult: waiting leader (response from server)
+        //Server is supposed to recognize when the leader has entered the game
+        panelFifth.add(fifthSubPanel);
+        fifthSubPanel.add(waitingLeader);
+        waitingLeader.setVerticalAlignment(waitingLeader.CENTER);
+        //Server does work here in order to switch to new Panel
+        //Once ready, switch to Panel 6
+
+        //Server is supposed to have a word identifier phrase in the "wordIdentification" panel
+        // and is supposed to record the user's suggestion that will then be used in the next panel
+        panelSixth.add(sixthSubPanel);
+        sixthSubPanel.setSize(300,500);
+        sixthSubPanel.add(wordIdentifcationInstructions);
+        sixthSubPanel.add(wordIdentification);
+        wordIdentification.add(wordClue);
+        //This text should change based on the server's response
+        wordClue.setText("A group of zebras"); //This text changes based on the server's response
+        wordIdentification.setBackground(Color.orange);
+        sixthSubPanel.add(suggestion);
+        Border suggestionBorder = BorderFactory.createTitledBorder("Your Suggestion");
+        suggestion.setBorder(suggestionBorder);
+        suggestion.add(suggestionBox);
+        //IMPORTANT: Record userSuggestion in server
+        String userSuggestion = suggestionBox.getText();
+        sixthSubPanel.add(buttonSubmitSuggestion);
+        buttonSubmitSuggestion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton a = (JButton) e.getSource();
+                if(a == buttonSubmitSuggestion) {
+                    layout.show(mainPanel, "7");
+                }
+            }
+        });
+
+        //Display the suggestions recorded in the previous panel in the text for
+        // the Radio Buttons
+        panelSeventh.add(seventhSubPanel);
+        seventhSubPanel.add(pickOptionDescription);
+        options.add(option1);
+        options.add(option2);
+        options.add(option3);
+        seventhSubPanel.add(option1);
+        option1.setText("A zippy-do"); //Replace with server's record
+        seventhSubPanel.add(option2);
+        option2.setText("A zig zag"); //Replace with server's record
+        seventhSubPanel.add(option3);
+        option3.setText("A dazzle"); //Replace with server's record
+        seventhSubPanel.add(buttonSubmitOption);
+        buttonSubmitOption.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                layout.show(mainPanel, "8");
+            }
+        });
+
+        //Server displays round results and overall results in each of the panels
+        // called "roundResult" and "overallResults"
+        panelEighth.add(eightSubPanel);
+        eightSubPanel.add(roundResult);
+        Border r = BorderFactory.createTitledBorder("Round Result");
+        roundResult.setBorder(r);
+        roundResult.setBackground(Color.green);
+        eightSubPanel.add(overallResults);
+        Border q = BorderFactory.createTitledBorder("Overall Results");
+        overallResults.setBorder(q);
+        overallResults.setBackground(Color.orange);
+        eightSubPanel.add(nextRound);
+        nextRound.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                layout.show(mainPanel, "6");
+            }
+        });
 
         mainPanel.setLayout(layout);
+        setTitle("FoilMaker");
         mainPanel.add(panelFirst, "1");
         mainPanel.add(panelSecond, "2");
         mainPanel.add(panelThird, "3");
         mainPanel.add(panelFourth, "4");
         mainPanel.add(panelFifth, "5");
-        setTitle("FoilMaker");
+        mainPanel.add(panelSixth, "6");
+        mainPanel.add(panelSeventh, "7");
+        mainPanel.add(panelEighth, "8");
         panelFirst.setBorder(p1);
         panelSecond.setBorder(rest);
         panelThird.setBorder(rest);
         panelFourth.setBorder(rest);
+        panelFifth.setBorder(rest);
+        panelSixth.setBorder(rest);
+        panelSeventh.setBorder(rest);
+        panelEighth.setBorder(rest);
 
-        add(mainPanel);
-        setLocation(640,480);
-        setMinimumSize(new Dimension(300,500));
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        pack();
+        frame.add(mainPanel);
+        frame.setSize(300,500);
+        frame.setTitle("FoilMaker");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         layout.show(mainPanel, "1");
-        setVisible(true);
+        frame.setResizable(true);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
