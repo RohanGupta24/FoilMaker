9929031a6823e636581c402a82cb02e6ca3e308e
Will Borland <wborland@purdue.edu>
2016-11-11 08:43:54 -0500

    Little Changes

--- foilmaker-01-files/FoilMaker/src/Controller.java
+++ foilmaker-01-files/FoilMaker/src/Controller.java
@@ -168,7 +168,7 @@ public class Controller{
 
 
     public boolean isGameStarted(){
-<<<<<<< HEAD
+
 
         String userToken = this.model.getUserToken();
         String gameToken = this.model.getGameToken();
@@ -196,9 +196,9 @@ public class Controller{
 
 
 
-=======
+
         this.view.setMessageBox("Press <Start Game> to start game");
->>>>>>> origin/master
+
         return true;
     }
 
@@ -235,7 +235,7 @@ public class Controller{
     }
 
     public boolean isSubmitSuggestion(){
-<<<<<<< HEAD
+
 
 
 
@@ -362,18 +362,11 @@ public class Controller{
 
 
 
-
-        return true;
-=======
         this.view.setMessageBox("Enter your suggestion");
         return false;
     }
 
-    public boolean isSubmitOption(){
-        this.view.setMessageBox("Pick your choice")
-        return false;
->>>>>>> origin/master
-    }
+
 
     public boolean isNextRound(){
         return false;
